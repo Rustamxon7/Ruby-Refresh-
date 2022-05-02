9744424aca9772fd01cb35147477dabe0fe1f7a3
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-02 13:52:36 +0500

    Data types

--- ruby.rb
+++ ruby.rb
@@ -13,11 +13,21 @@
 # puts " /___|"
 
 # WORKING WITH VARIABLES
-character_name = "John"
-character_age = "35"
+# character_name = "John"
+# character_age = "35"
 
-puts ("There once was a man named " + character_name)
-puts ("he was " + character_age + " years old.")
-character_name = "Tom"
-puts ("He realy liked the name " + character_name)
-puts ("but didn't like being " + character_age)
+# puts ("There once was a man named " + character_name)
+# puts ("he was " + character_age + " years old.")
+# character_name = "Tom"
+# puts ("He realy liked the name " + character_name)
+# puts ("but didn't like being " + character_age)
+
+# DATA TYPES
+
+name = "Mike"
+age = 75
+occupation = "programmer"
+gpa = 3.2443523
+ismale = true
+istale = false
+flaws = nil
