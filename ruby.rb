adc9a684cc0759fdc37d5e41bdb3409a05a0bad9
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-04 09:53:50 +0500

    If Statements (conditions)

--- ruby.rb
+++ ruby.rb
@@ -132,15 +132,29 @@
 
 # If Statements
 
-ismale = false
-istall = true
+# ismale = false
+# istall = true
 
-if ismale and istall
-  puts "You are tall male"
-elsif ismale and !istall
-  puts "You are a short male"
-elsif !ismale and istall
-  puts "You are not male but are tall"
-else
-  puts "You either not male or tall or both"
+# if ismale and istall
+#   puts "You are tall male"
+# elsif ismale and !istall
+#   puts "You are a short male"
+# elsif !ismale and istall
+#   puts "You are not male but are tall"
+# else
+#   puts "You either not male or tall or both"
+# end
+
+# If Statements (conditions)
+
+def  max(num1, num2, num3)
+  if num1 >= num2 and num1 >= num3
+    return num1 
+  elsif num2 >= num1 and num2 >= num3
+    return num2 
+  else
+    return num3
+  end
 end
+
+puts max(1, 2, 3)
