f83784fedc20fa5b9235bec514bea224e9b02b99
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-04 10:18:06 +0500

    Case expressions

--- ruby.rb
+++ ruby.rb
@@ -161,23 +161,52 @@
 
 # Building a Better Calculator
 
-puts "Enter first number: "
-num1 = gets.chomp().to_f
+# puts "Enter first number: "
+# num1 = gets.chomp().to_f
 
-puts "Enter first operator: "
-op = gets.chomp()
+# puts "Enter first operator: "
+# op = gets.chomp()
 
-puts "Enter second number: "
-num2 = gets.chomp().to_f
+# puts "Enter second number: "
+# num2 = gets.chomp().to_f
 
-if op == "+"
-  puts (num1 + num2)
-elsif op == "-"
-  puts (num1 - num2)
-elsif op == "/"
-  puts (num1 / num2)
-elsif op == "*"
-  puts (num1 * num2)
-else
-  puts "Invalid operator"
+# if op == "+"
+#   puts (num1 + num2)
+# elsif op == "-"
+#   puts (num1 - num2)
+# elsif op == "/"
+#   puts (num1 / num2)
+# elsif op == "*"
+#   puts (num1 * num2)
+# else
+#   puts "Invalid operator"
+# end
+
+# Case Expressions
+
+def get_day_name(day)
+  day_name = ""
+
+  case day
+  when "mon"
+    day_name = "Monday"
+  when "tue"
+    day_name = "Tuesday"
+  when "wed"
+    day_name = "Wednesday"
+  when "thu"
+    day_name ="Thursday"
+  when "fri"
+    day_name = "Friday"
+  when "sat"
+    day_name = "Saturday"
+  when "sun"
+    day_name = "Sunday"
+  else
+    day_name = "Invalid abbriviation"
+  end
+
+  return day_name
 end
+
+puts get_day_name("sun")
