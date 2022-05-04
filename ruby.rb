a3b1461ca3fa84e877c6966db948dc2b1c6d04b9
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-04 10:08:07 +0500

    Building a Better Calculator

--- ruby.rb
+++ ruby.rb
@@ -147,14 +147,37 @@
 
 # If Statements (conditions)
 
-def  max(num1, num2, num3)
-  if num1 >= num2 and num1 >= num3
-    return num1 
-  elsif num2 >= num1 and num2 >= num3
-    return num2 
-  else
-    return num3
-  end
-end
+# def  max(num1, num2, num3)
+#   if num1 >= num2 and num1 >= num3
+#     return num1 
+#   elsif num2 >= num1 and num2 >= num3
+#     return num2 
+#   else
+#     return num3
+#   end
+# end
 
-puts max(1, 2, 3)
+# puts max(1, 2, 3)
+
+# Building a Better Calculator
+
+puts "Enter first number: "
+num1 = gets.chomp().to_f
+
+puts "Enter first operator: "
+op = gets.chomp()
+
+puts "Enter second number: "
+num2 = gets.chomp().to_f
+
+if op == "+"
+  puts (num1 + num2)
+elsif op == "-"
+  puts (num1 - num2)
+elsif op == "/"
+  puts (num1 / num2)
+elsif op == "*"
+  puts (num1 * num2)
+else
+  puts "Invalid operator"
+end
