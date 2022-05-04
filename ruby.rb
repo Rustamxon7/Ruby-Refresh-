93ff1a8ed649e45d83f699af4efaefb9dbc0e8b3
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-04 10:46:38 +0500

    Guessing game

--- ruby.rb
+++ ruby.rb
@@ -213,8 +213,35 @@
 
 # While Loops
 
-index = 1
-while index <= 8
-  puts index
-  index += 1
+# index = 1
+# while index <= 8
+#   puts index
+#   index += 1
+# end
+
+# Building a Guessing Game
+
+secret_word = "guessing"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
+
+
+
+while guess != secret_word and !out_of_guesses
+  if guess_count < guess_limit
+    puts guess_left = guess_limit - guess_count
+    puts "Enter guess: " 
+    guess = gets.chomp()
+    guess_count += 1
+  else
+    out_of_guesses = true
+  end
+end
+
+if out_of_guesses
+  puts "You Lose 😔"
+else
+  puts "You Won!"
 end
