0758b4e58fee3831ae67931638337d6bd372f547
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-05 13:56:29 +0500

    Modules

--- ruby.rb
+++ ruby.rb
@@ -432,29 +432,36 @@
 
 # Inheritance
 
-class Chef
-  def make_chicken
-    puts "The chef makes chicken"
-  end
-  def make_salad
-    puts "The chef makes salad"
-  end
-  def make_special_dish
-    puts "The chef makes bbq ribs"
-  end
-end
+# class Chef
+#   def make_chicken
+#     puts "The chef makes chicken"
+#   end
+#   def make_salad
+#     puts "The chef makes salad"
+#   end
+#   def make_special_dish
+#     puts "The chef makes bbq ribs"
+#   end
+# end
 
-class ItalianChef < Chef
-  def make_special_dish
-    puts "The chef makes eggplant parm"
-  end
-  def make_pasta
-    print "The chef makes pasta"
-  end
-end
+# class ItalianChef < Chef
+#   def make_special_dish
+#     puts "The chef makes eggplant parm"
+#   end
+#   def make_pasta
+#     print "The chef makes pasta"
+#   end
+# end
 
-chef = Chef.new()
-chef.make_special_dish
+# chef = Chef.new()
+# chef.make_special_dish
 
-italian_chef = ItalianChef.new()
-italian_chef.make_special_dish
+# italian_chef = ItalianChef.new()
+# italian_chef.make_special_dish
+
+# Modules
+
+require_relative "useful_tools.rb"
+
+include Tools
+Tools.saybye('Bob')
---
+++ useful_tools.rb
@@ -0,0 +1,10 @@
+module Tools
+  def sayhi(name)
+    puts "sayhi: #{name}"
+  end
+
+  def saybye(name)
+    puts "saybye: #{name}"
+  end
+end
+
