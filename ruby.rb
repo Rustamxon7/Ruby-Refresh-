ac97f92b00bf2f392931e09c769359b5b3250d9c
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-05-05 13:50:28 +0500

    Inheritance

--- ruby.rb
+++ ruby.rb
@@ -397,35 +397,64 @@
 
 # Building a Quiz
 
-class Question
-  attr_accessor :prompt, :answer
-  def initialize(prompt, answer)
-    @prompt = prompt
-    @answer = answer
+# class Question
+#   attr_accessor :prompt, :answer
+#   def initialize(prompt, answer)
+#     @prompt = prompt
+#     @answer = answer
+#   end
+# end
+
+# p1 = "What color are apples?\n(a)red\n(b)purple\n(c)orange\n(d)"
+# p2 = "What color are bananas?\n(a)pink\n(b)red\n(c)yellow"
+# p3 = "What color are pears?\n(a)yellow\n(b)green\n(c)orange"
+
+# questions = [
+#   Question.new(p1, "a"),
+#   Question.new(p2, "c"),
+#   Question.new(p3, "b")
+# ]
+
+# def run_test(questions)
+#   answer = ""
+#   score = 0
+#   for question in questions
+#     puts question.prompt
+#     answer = gets.chomp()
+#     if answer == question.answer
+#       score += 1
+#     end
+#   end
+#   puts ("You got " + score.to_s + "/" + questions.length().to_s)
+# end
+
+# run_test(questions)
+
+# Inheritance
+
+class Chef
+  def make_chicken
+    puts "The chef makes chicken"
+  end
+  def make_salad
+    puts "The chef makes salad"
+  end
+  def make_special_dish
+    puts "The chef makes bbq ribs"
   end
 end
 
-p1 = "What color are apples?\n(a)red\n(b)purple\n(c)orange\n(d)"
-p2 = "What color are bananas?\n(a)pink\n(b)red\n(c)yellow"
-p3 = "What color are pears?\n(a)yellow\n(b)green\n(c)orange"
-
-questions = [
-  Question.new(p1, "a"),
-  Question.new(p2, "c"),
-  Question.new(p3, "b")
-]
-
-def run_test(questions)
-  answer = ""
-  score = 0
-  for question in questions
-    puts question.prompt
-    answer = gets.chomp()
-    if answer == question.answer
-      score += 1
-    end
+class ItalianChef < Chef
+  def make_special_dish
+    puts "The chef makes eggplant parm"
   end
-  puts ("You got " + score.to_s + "/" + questions.length().to_s)
+  def make_pasta
+    print "The chef makes pasta"
+  end
 end
 
-run_test(questions)
+chef = Chef.new()
+chef.make_special_dish
+
+italian_chef = ItalianChef.new()
+italian_chef.make_special_dish
